d3a517e1f12ac936813109b1a35a7c17ee735442
Çağrı Uslu <uslu.cagri@gmail.com>
2021-06-21 00:19:59 +0200

    Added TODO

--- src/Pathfinder.c
+++ src/Pathfinder.c
@@ -1,9 +1,11 @@
 #include "Pathfinder.h"
 
 int PathfinderMapFromLevel(PathfinderMap* pm, Level* level) {
-	
+	// TODO
+	// Iterate over object
+	// Check their category using body->fixture->filter->category
+	// Iterate over shapes and combine AABBs
+	// Mark all grids that collide with AABB
+	// Put marked ones in a map
 	return 0;
 }
-
-// iterate over objects, test (or find out which grid locations it occupies)
-// put them in a integer map
