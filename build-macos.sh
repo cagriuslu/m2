c8f49f3120e39ac096efa986709b4c64c308564e
Çağrı Uslu <uslu.cagri@gmail.com>
2021-11-28 11:47:34 +0100

    Remove MacOS build script

--- build-macos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-clang   -g --std=c11   -c src/*/*.c src/*.c   -I/usr/local/include/SDL2
-clang++ -g --std=c++11 -c src/*.cpp -I/usr/local/include/SDL2 -I/usr/local/include/box2d
-clang++ -g --std=c++11 -o bin/cgame *.o -lbox2d -lSDL2_image -lSDL2_ttf -lSDL2 -lm
-rm *.o
