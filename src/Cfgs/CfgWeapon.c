7bca1a29ba534fafda05884e05cc182ddaba2c16
Çağrı Uslu <uslu.cagri@gmail.com>
2021-12-18 12:34:33 +0100

    Created CfgWeapon

---
+++ src/Cfgs/CfgWeapon.c
@@ -0,0 +1,12 @@
+#include "../Cfg.h"
+
+const CfgRangedWeapon CFG_RANGEDWPN_GUN; // Default
+const CfgRangedWeapon CFG_RANGEDWPN_MACHINEGUN; // Fast, thus powerful
+const CfgRangedWeapon CFG_RANGEDWPN_SHOTGUN; // Slow but powerful
+const CfgRangedWeapon CFG_RANGEDWPN_BOWNARRAY; // Slow, but piercing, thus default
+const CfgMeleeWeapon CFG_MELEEWPN_BAT; // Default
+const CfgMeleeWeapon CFG_MELEEWPN_SWORD; // Slow but powerful
+const CfgMeleeWeapon CFG_MELEEWPN_SPEAR; // Slow, High damage and pierce
+const CfgMeleeWeapon CFG_MELEEWPN_DAGGER; // Fast, thus powerful
+const CfgExplosiveWeapon CFG_EXPLOSIVEWPN_GRENADE;
+const CfgExplosiveWeapon CFG_EXPLOSIVEWPN_GRENADELAUNCHER;
