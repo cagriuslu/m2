577364e31e737f935ac9f6e703a791599188f4fb
Çağrı Uslu <uslu.cagri@gmail.com>
2022-01-04 21:30:10 +0100

    Modified level

--- src/Cfgs/CfgLevel.c
+++ src/Cfgs/CfgLevel.c
@@ -14,28 +14,19 @@
 #define _skl  {.gndTile = &CFG_GNDTXTR_DEFAULT, .chr = &CFG_CHARACTER_SKELETON000}
 
 const CfgLevelTile sp000_tiles[] = {
-	_,		_,		_,		_,		_,		_,		_,		_,		_,		_,
-	_,		clfTop,	clfTop,	clfTop,	clfTop,	clfTop,	_,		_,		_,		_,
-	_plyr,	_,		_,		_,		_,		_,		clfRig,	_,		_,		_,
-	_,		clfBot,	clfBot,	clfBot,	cBotLef,_,		clfRig,	_,		_,		_,
-	_,		_,		_,		_,		clfLef,	_,		clfRig,	_,		_,		_,
-	_,		_,		_,		_,		clfLef,	_,		clfRig,	_,		_,		_,
-	_,		_,		_,		_,		clfLef,	_,		clfRig,	_,		_,		_,
-	_,		_,		_,		_,		_,		_,		_,		_,		_,		_,
-	_,		_,		_,		_,		_skl,	_,		_skl,	_,		_,		_,
-	_,		_,		_,		_,		_,		_,		_,		_,		_,		_
+	_,		clfTop,	clfTop,	clfTop,	clfTop,	clfTop,	clfTop,	clfTop,	clfTop,	clfTop,
+	clfLef,	_plyr,	_,		_,		_,		_,		_,		_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_,		_,		_,		_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_,		_,		_,		_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_,		_,		_,		_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_,		_,		_,		_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_skl,	_skl,	_skl,	_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_skl,	_skl,	_skl,	_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_skl,	_skl,	_skl,	_skl,	_skl,	_skl,
+	clfLef,	_,		_,		_,		_,		_,		_,		_,		_,		_,
 };
 const CfgLevel CFG_LVL_SP000 = {
 		.tiles = sp000_tiles,
 		.w = 10,
 		.h = 10
 };
-#undef _
-#undef clfTop
-#undef clfRig
-#undef clfBot
-#undef clfLef
-#undef cTopRig
-#undef cTopLef
-#undef cBotRig
-#undef cBotLef
