94261351df155cb47946d6eb74eaba3d0d549c7a
Çağrı Uslu <uslu.cagri@gmail.com>
2022-08-12 22:57:10 +0200

    Backup

--- include/m2/Rtree.h
+++ include/m2/Rtree.h
@@ -4,6 +4,13 @@
 #include "AABB2f.h"
 #include "Pool.hh"
 
+// Binary tree where each node is an AABB, as you go up the levels, the AABBs get merged. In the highest level, you have the AABB that covers the whole world.
+// Instead of binary, we should use k=4 because it has potential to be more efficient
+// We must make sure that the objects that are closer to each other form an AABB
+// Is it perfectly possible to have intersections among AABBs, the important thing is the centers of the objects are grouped correctly.
+// It is actually not a must, but we should have a good enough algorithm for grouping.
+// If the grouping accumulates errors, we would start searching all objects
+
 namespace m2 {
 	using RtreeID = ID;
 
