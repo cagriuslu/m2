43df34f96a2a4e1cf42a2b03193df4ba9199c5e1
Çağrı Uslu <uslu.cagri@gmail.com>
2022-12-28 22:33:28 +0100

    Inline Vec2f

--- include/m2/Vec2f.h
+++ include/m2/Vec2f.h
@@ -12,58 +12,60 @@ namespace m2 {
 	struct Vec2i;
 
 	struct Vec2f {
-		float x, y;
+		float x{}, y{};
 
-		Vec2f();
-		Vec2f(float x, float y);
-		Vec2f(int x, int y);
-		Vec2f(unsigned x, unsigned y);
-		explicit Vec2f(const Vec2i& v);
-		explicit Vec2f(const b2Vec2& v);
-		explicit Vec2f(const pb::Dim2f& v);
-		explicit Vec2f(const pb::Vec2f& v);
-		explicit Vec2f(const pb::Vec2i& v);
+		inline Vec2f() = default;
+		inline Vec2f(float x, float y) : x(x), y(y) {}
+		inline Vec2f(int x, int y) : x((float)x), y((float)y) {}
+		inline Vec2f(unsigned x, unsigned y) : x((float)x), y((float)y) {}
+		inline explicit Vec2f(const b2Vec2& v) : Vec2f(v.x, v.y) {}
+		inline explicit Vec2f(const pb::Dim2f& v) : Vec2f(v.w(), v.h()) {}
+		inline explicit Vec2f(const pb::Vec2f& v) : Vec2f(v.x(), v.y()) {}
+		inline explicit Vec2f(const pb::Vec2i& v) : Vec2f(v.x(), v.y()) {}
 
-		Vec2f operator+(const Vec2f& rhs) const;
-		Vec2f operator+(const float& rhs) const;
-		Vec2f& operator+=(const Vec2f& rhs);
-		Vec2f operator-(const Vec2f& rhs) const;
-		Vec2f operator-(const float& rhs) const;
-		Vec2f operator*(const float& rhs) const;
-		Vec2f operator/(const float& rhs) const;
-		Vec2f operator/(const int& rhs) const;
-		Vec2f operator/(const unsigned& rhs) const;
-		bool operator==(const Vec2f& other) const;
-		explicit operator bool() const;
-		explicit operator b2Vec2() const;
+		inline Vec2f operator+(const Vec2f& rhs) const { return {x + rhs.x, y + rhs.y}; }
+		inline Vec2f operator+(const float& rhs) const { return {x + rhs, y + rhs}; }
+		inline Vec2f& operator+=(const Vec2f& rhs) { x += rhs.x; y += rhs.y; return *this; }
+		inline Vec2f operator-(const Vec2f& rhs) const { return {x - rhs.x, y - rhs.y}; }
+		inline Vec2f operator-(const float& rhs) const { return {x - rhs, y - rhs}; }
+		inline Vec2f operator*(const float& rhs) const { return {x * rhs, y * rhs}; }
+		inline Vec2f operator/(const float& rhs) const { return {x / rhs, y / rhs}; }
+		inline Vec2f operator/(const int& rhs) const { return {x / (float)rhs, y / (float)rhs}; }
+		inline Vec2f operator/(const unsigned& rhs) const { return {x / (float)rhs, y / (float)rhs}; }
+		inline bool operator==(const Vec2f& other) const { return (x == other.x) && (y == other.y); }
+		inline explicit operator bool() const { return (x != 0.0f) && (y != 0.0f); }
+		inline explicit operator b2Vec2() const { return b2Vec2{x, y}; }
 
-		[[nodiscard]] bool is_nan() const;
-		[[nodiscard]] bool is_small(float tolerance) const;
-		[[nodiscard]] bool is_near(const Vec2f& other, float tolerance) const;
-		[[nodiscard]] float length() const;
-		[[nodiscard]] float length_sq() const;
-		[[nodiscard]] float distance(const Vec2f& other) const;
-		[[nodiscard]] float distance_sq(const Vec2f& other) const;
-		[[nodiscard]] float angle_rads() const;
+		[[nodiscard]] inline bool is_nan() const { return isnan(x) || isnan(y); }
+		[[nodiscard]] inline bool is_near(const Vec2f& other, float tolerance) const { return fabsf(other.x - x) <= tolerance && fabsf(other.y - y) <= tolerance; }
+		[[nodiscard]] inline bool is_small(float tolerance) const { return is_near({}, tolerance); }
+		[[nodiscard]] inline float length_sq() const { return x * x + y * y; }
+		[[nodiscard]] inline float length() const { return sqrt(length_sq()); }
+		[[nodiscard]] inline float distance_sq(const Vec2f& other) const { return (other - *this).length_sq(); }
+		[[nodiscard]] inline float distance(const Vec2f& other) const { return (other - *this).length(); }
+		[[nodiscard]] inline float angle_rads() const { return atan2(y, x); }
 
-		[[nodiscard]] Vec2f normalize() const;
-		[[nodiscard]] Vec2f ln() const;
-		[[nodiscard]] Vec2f round() const;
-		[[nodiscard]] Vec2i iround() const;
-		[[nodiscard]] Vec2f floor() const;
-		[[nodiscard]] Vec2i ifloor() const;
-		[[nodiscard]] Vec2f ceil() const;
-		[[nodiscard]] Vec2i iceil() const;
-		[[nodiscard]] Vec2f floor_length(float len) const;
-		[[nodiscard]] Vec2f with_length(float len) const;
-		[[nodiscard]] Vec2f ceil_length(float len) const;
-		[[nodiscard]] Vec2f lerp(const Vec2f& to, float ratio) const;
+		[[nodiscard]] inline Vec2f normalize() const { float len = length(); return len != 0.0f ? Vec2f{x / len, y / len} : Vec2f{}; }
+		[[nodiscard]] inline Vec2f ln() const { return {logf(x), logf(y)}; }
+		[[nodiscard]] inline Vec2f round() const { return {roundf(x), roundf(y)}; }
+		[[nodiscard]] inline Vec2f floor() const { return {floorf(x), floorf(y)}; }
+		[[nodiscard]] inline Vec2f ceil() const { return {ceilf(x), ceilf(y)}; }
+		[[nodiscard]] inline Vec2f with_length(float len) const { return normalize() * len; }
+		[[nodiscard]] inline Vec2f floor_length(float len) const { return length() < len ? with_length(len) : *this; }
+		[[nodiscard]] inline Vec2f ceil_length(float len) const { return len < length() ? with_length(len) : *this; }
+		[[nodiscard]] inline Vec2f lerp(const Vec2f& to, float ratio) const { return *this + (to - *this) * ratio; }
 
 		/// Order of corners: Bottom-right, Bottom-left, Top-left, Top-right
 		[[nodiscard]] std::array<Vec2f, 4> aabb_corners(float aabb_radius) const;
 
-		static Vec2f nan();
-		static Vec2f from_angle(float rads);
+		// Vec2i
+		explicit Vec2f(const Vec2i& v);
+		[[nodiscard]] Vec2i iround() const;
+		[[nodiscard]] Vec2i ifloor() const;
+		[[nodiscard]] Vec2i iceil() const;
+
+		inline static Vec2f nan() { return {NAN, NAN}; }
+		inline static Vec2f from_angle(float rads) { return {cosf(rads), sinf(rads)}; }
 	};
 
 	std::string to_string(const m2::Vec2f&);
--- src/Vec2f.cc
+++ src/Vec2f.cc
@@ -4,138 +4,19 @@
 #include <sstream>
 #include <iomanip>
 
-m2::Vec2f::Vec2f() : x(0.0f), y(0.0f) {}
-m2::Vec2f::Vec2f(float x, float y) : x(x), y(y) {}
-m2::Vec2f::Vec2f(int x, int y) : x(static_cast<float>(x)), y(static_cast<float>(y)) {}
-m2::Vec2f::Vec2f(unsigned x, unsigned y) : x(static_cast<float>(x)), y(static_cast<float>(y)) {}
 m2::Vec2f::Vec2f(const Vec2i& v) : Vec2f(v.x, v.y) {}
-m2::Vec2f::Vec2f(const b2Vec2& v) : Vec2f(v.x, v.y) {}
-m2::Vec2f::Vec2f(const pb::Dim2f& v) : Vec2f(v.w(), v.h()) {}
-m2::Vec2f::Vec2f(const pb::Vec2f& v) : Vec2f(v.x(), v.y()) {}
-m2::Vec2f::Vec2f(const pb::Vec2i& v) : Vec2f(v.x(), v.y()) {}
-
-m2::Vec2f m2::Vec2f::operator+(const Vec2f& rhs) const {
-	return {x + rhs.x, y + rhs.y};
-}
-m2::Vec2f m2::Vec2f::operator+(const float& rhs) const {
-	return {x + rhs, y + rhs};
-}
-m2::Vec2f& m2::Vec2f::operator+=(const Vec2f& rhs) {
-	x += rhs.x;
-	y += rhs.y;
-	return *this;
-}
-m2::Vec2f m2::Vec2f::operator-(const Vec2f& rhs) const {
-	return {x - rhs.x, y - rhs.y};
-}
-m2::Vec2f m2::Vec2f::operator-(const float& rhs) const {
-	return {x - rhs, y - rhs};
-}
-m2::Vec2f m2::Vec2f::operator*(const float& rhs) const {
-	return {x * rhs, y * rhs};
-}
-m2::Vec2f m2::Vec2f::operator/(const float& rhs) const {
-	return {x / rhs, y / rhs};
-}
-m2::Vec2f m2::Vec2f::operator/(const int& rhs) const {
-	return {x / static_cast<float>(rhs), y / static_cast<float>(rhs)};
-}
-m2::Vec2f m2::Vec2f::operator/(const unsigned& rhs) const {
-	return {x / static_cast<float>(rhs), y / static_cast<float>(rhs)};
-}
-bool m2::Vec2f::operator==(const Vec2f& other) const {
-	return (x == other.x) && (y == other.y);
-}
-m2::Vec2f::operator bool() const {
-	return (x != 0.0f) && (y != 0.0f);
-}
-m2::Vec2f::operator b2Vec2() const {
-	return b2Vec2{ x, y };
-}
-
-bool m2::Vec2f::is_nan() const {
-	return isnan(x) || isnan(y);
-}
-bool m2::Vec2f::is_small(float tolerance) const {
-	return is_near({}, tolerance);
-}
-bool m2::Vec2f::is_near(const Vec2f& other, float tolerance) const {
-	return fabsf(other.x - x) <= tolerance && fabsf(other.y - y) <= tolerance;
-}
-float m2::Vec2f::length() const {
-	return sqrt(length_sq());
-}
-float m2::Vec2f::length_sq() const {
-	return x * x + y * y;
-}
-float m2::Vec2f::distance(const Vec2f& other) const {
-	return (other - *this).length();
-}
-float m2::Vec2f::distance_sq(const Vec2f& other) const {
-	return (other - *this).length_sq();
-}
-float m2::Vec2f::angle_rads() const {
-	return atan2(y, x);
-}
-
-m2::Vec2f m2::Vec2f::normalize() const {
-	float len = length();
-	if (len != 0.0f) {
-		return {x / len, y / len};
-	}
-	return {};
-}
-m2::Vec2f m2::Vec2f::ln() const {
-	return {logf(x), logf(y)};
-}
-m2::Vec2f m2::Vec2f::round() const {
-	return {roundf(x), roundf(y)};
-}
 m2::Vec2i m2::Vec2f::iround() const {
 	return Vec2i{this->round()};
-}
-m2::Vec2f m2::Vec2f::floor() const {
-	return {floorf(x), floorf(y)};
 }
 m2::Vec2i m2::Vec2f::ifloor() const {
 	return Vec2i{this->floor()};
 }
-m2::Vec2f m2::Vec2f::ceil() const {
-	return {ceilf(x), ceilf(y)};
-}
 m2::Vec2i m2::Vec2f::iceil() const {
 	return Vec2i{this->ceil()};
-}
-m2::Vec2f m2::Vec2f::floor_length(float len) const {
-	if (length() < len) {
-		return with_length(len);
-	} else {
-		return *this;
-	}
-}
-m2::Vec2f m2::Vec2f::with_length(float len) const {
-	return normalize() * len;
-}
-m2::Vec2f m2::Vec2f::ceil_length(float len) const {
-	if (len < length()) {
-		return with_length(len);
-	} else {
-		return *this;
-	}
-}
-m2::Vec2f m2::Vec2f::lerp(const Vec2f& to, float ratio) const {
-	return (to - *this) * ratio + *this;
 }
 
 std::array<m2::Vec2f, 4> m2::Vec2f::aabb_corners(float aabb_radius) const {
 	return {Vec2f{x + aabb_radius, y + aabb_radius}, Vec2f{x - aabb_radius, y + aabb_radius}, Vec2f{x - aabb_radius, y - aabb_radius}, Vec2f{x + aabb_radius, y - aabb_radius}};
-}
-
-m2::Vec2f m2::Vec2f::nan() {
-	return {NAN, NAN};
-}
-m2::Vec2f m2::Vec2f::from_angle(float rads) {
-	return {cosf(rads), sinf(rads)};
 }
 
 std::string m2::to_string(const m2::Vec2f& v) {
