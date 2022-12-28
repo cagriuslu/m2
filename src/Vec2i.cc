2297952be2bc4fee0128dc86be937a466b92c37a
Çağrı Uslu <uslu.cagri@gmail.com>
2022-12-28 22:55:20 +0100

    Inline Vec2i

--- src/Vec2i.cc
+++ src/Vec2i.cc
@@ -3,48 +3,7 @@
 #include <unordered_map>
 #include <sstream>
 
-m2::Vec2i::Vec2i() : x(0), y(0) {}
-m2::Vec2i::Vec2i(int32_t x, int32_t y) : x(x), y(y) {}
-m2::Vec2i::Vec2i(float x, float y) : x(static_cast<int32_t>(x)), y(static_cast<int32_t>(y)) {}
 m2::Vec2i::Vec2i(const m2::Vec2f& v) : Vec2i(v.x, v.y) {}
-m2::Vec2i::Vec2i(const pb::Vec2i& v) : Vec2i(v.x(), v.y()) {}
-
-m2::Vec2i m2::Vec2i::operator+(const Vec2i& rhs) const {
-	return {x + rhs.x, y + rhs.y};
-}
-m2::Vec2i m2::Vec2i::operator-(const Vec2i& rhs) const {
-	return {x - rhs.x, y - rhs.y};
-}
-bool m2::Vec2i::operator==(const Vec2i& other) const {
-	return x == other.x && y == other.y;
-}
-m2::Vec2i::operator bool() const {
-	return (x || y);
-}
-
-bool m2::Vec2i::is_near(const Vec2i& other, int tolerance) const {
-	return abs(other.x - x) <= tolerance && abs(other.y - y) <= tolerance;
-}
-bool m2::Vec2i::in_nonnegative() const {
-	return 0 <= x && 0 <= y;
-}
-float m2::Vec2i::length() const {
-	return sqrt(length_sq());
-}
-float m2::Vec2i::length_sq() const {
-	return (float)x * (float)x + (float)y * (float)y;
-}
-float m2::Vec2i::distance(const Vec2i& other) const {
-	return (other - *this).length();
-}
-int m2::Vec2i::manhattan_distance(const Vec2i& other) const {
-	return abs(other.x - x) + abs(other.y - y);
-}
-
-size_t m2::Vec2iHash::operator()(const Vec2i& a) const {
-	uint64_t packed = static_cast<uint64_t>(a.x) | (static_cast<uint64_t>(a.y) << 32);
-	return std::hash<uint64_t>{}(packed);
-}
 
 std::string m2::to_string(const m2::Vec2i& v) {
 	std::stringstream ss;
