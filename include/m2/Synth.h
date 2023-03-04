b3c1af1d298455614ec83666a7591b8d680dec45
Çağrı Uslu <uslu.cagri@gmail.com>
2023-03-04 22:06:49 +0100

    Add comments

--- include/m2/Synth.h
+++ include/m2/Synth.h
@@ -12,19 +12,29 @@ namespace m2 {
 	using SynthBpm = uint32_t;
 
 	namespace synth {
-		/// Returns the number of samples for the given number of beats for a given BPM.
-		/// Integer division is used internally, thus rounding occurs towards down.
+		/// Returns the number of samples for the given number of beats for a given BPM
+		/// Integer division is used internally, thus rounding occurs towards down
 		size_t beat_sample_count(SynthBpm bpm, const pb::Rational& beats, unsigned sample_rate = 48000);
+		/// Returns the number of steps for the given number of beats if the max_denominator is as given
 		size_t beat_step_count(const pb::Rational& beats, int64_t max_denominator);
 
+		/// Returns the number of beats in the track
 		Rational track_beat_count(const pb::SynthTrack& track);
+		/// Returns the max_denominator of the track
+		/// max_denominator is the finest division of the beats in the track
 		int64_t track_max_denominator(const pb::SynthTrack& track);
+		/// Returns the number of steps in the track
 		size_t track_step_count(const pb::SynthTrack& track);
+		/// Returns the number of samples for the given track for a given BPM
 		size_t track_sample_count(SynthBpm bpm, const pb::SynthTrack& track, unsigned sample_rate = 48000);
 
+		/// Returns the number of beats in the song
 		Rational song_beat_count(const pb::SynthSong& song);
+		/// Returns the max_denominator of the song
 		int64_t song_max_denominator(const pb::SynthSong& song);
+		/// Returns the number of steps in the song
 		size_t song_step_count(const pb::SynthSong& song);
+		/// Returns the number of samples for the given song
 		size_t song_sample_count(const pb::SynthSong& song, unsigned sample_rate = 48000);
 
 		namespace internal {
