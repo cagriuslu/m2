ce56619185d1a74f25f45af941ea35ed582b34e5
Çağrı Uslu <uslu.cagri@gmail.com>
2023-05-10 22:02:52 +0200

    RPG UI improvement

--- game/RPG/src/ProxyUi.cc
+++ game/RPG/src/ProxyUi.cc
@@ -5,7 +5,7 @@
 
 using namespace m2::ui;
 
-static Blueprint::Widget::Variant pause_menu_variant_1 = Blueprint::Widget::Text{
+static Blueprint::Widget::Variant resume_button = Blueprint::Widget::Text{
 	.initial_text = "Resume",
 	.action_callback = []() {
 		LOG_DEBUG("Resume button pressed");
@@ -13,7 +13,7 @@ static Blueprint::Widget::Variant pause_menu_variant_1 = Blueprint::Widget::Text
 	},
 	.kb_shortcut = SDL_SCANCODE_R
 };
-static Blueprint::Widget::Variant pause_menu_variant_2 = Blueprint::Widget::Text{
+static Blueprint::Widget::Variant quit_button = Blueprint::Widget::Text{
 	.initial_text = "Quit",
 	.action_callback = []() {
 		return m2::ui::Action::QUIT;
@@ -28,43 +28,44 @@ const Blueprint pause_menu_blueprint = {
 		Blueprint::Widget{
 			.x = 75, .y = 42, .w = 10, .h = 6,
 			.border_width_px = 1,
-			.variant = pause_menu_variant_1
+			.variant = resume_button
 		},
 		Blueprint::Widget{
 			.x = 75, .y = 78, .w = 10, .h = 6,
 			.border_width_px = 1,
-			.variant = pause_menu_variant_2
+			.variant = quit_button
 		}
 	}
 };
 
-static Blueprint::Widget::Variant left_hud_variant_1 = Blueprint::Widget::Text{
+static Blueprint::Widget::Variant hp_label = Blueprint::Widget::Text{
 	.initial_text = "HP"
 };
-static Blueprint::Widget::Variant left_hud_variant_2 = Blueprint::Widget::Text{
-	.initial_text = "100",
-	.update_callback = []() -> std::pair<Action,std::string> {
-		// Lookup player
-		if (LEVEL.player_id) {
-			float hp = LEVEL.player()->character().get_resource(m2g::pb::RESOURCE_HP);
-			return {Action::CONTINUE, std::to_string((int)round(hp * 100.0f))};
-		} else {
-			return {Action::CONTINUE, {}};
+static Blueprint::Widget::Variant hp_progress_bar = Blueprint::Widget::ProgressBar{
+		.initial_progress = 1.0f,
+		.bar_color = SDL_Color{255, 0, 0, 255},
+		.update_callback = []() -> float {
+			if (LEVEL.player()) {
+				return LEVEL.player()->character().get_resource(m2g::pb::RESOURCE_HP);
+			}
+			return 0.0f;
 		}
-	}
 };
-static Blueprint::Widget::Variant left_hud_variant_3 = Blueprint::Widget::Text{
+static Blueprint::Widget::Variant dash_label = Blueprint::Widget::Text{
 	.initial_text = "DASH"
 };
-static Blueprint::Widget::Variant left_hud_variant_4 = Blueprint::Widget::ProgressBar{
+static Blueprint::Widget::Variant dash_progress_bar = Blueprint::Widget::ProgressBar{
 	.initial_progress = 1.0f,
 	.bar_color = SDL_Color{255, 255, 0, 255},
 	.update_callback = []() {
-		if (LEVEL.player_id) {
-			float counter = LEVEL.player()->character().get_resource(m2g::pb::RESOURCE_DASH_ENERGY);
-			float cooldown = 2.0f;
-			counter = (cooldown <= counter) ? cooldown : counter;
-			return counter / cooldown;
+		if (LEVEL.player()) {
+			// Check if player has DASH capability
+			if (LEVEL.player()->character().has_item(m2g::pb::ITEM_REUSABLE_DASH_2S)) {
+				float counter = LEVEL.player()->character().get_resource(m2g::pb::RESOURCE_DASH_ENERGY);
+				float cooldown = 2.0f;
+				counter = (cooldown <= counter) ? cooldown : counter;
+				return counter / cooldown;
+			}
 		}
 		return 0.0f;
 	}
@@ -75,20 +76,22 @@ const Blueprint left_hud_blueprint = {
 	.widgets = {
 		Blueprint::Widget{
 			.x = 4, .y = 50, .w = 11, .h = 2,
-			.variant = left_hud_variant_1
+			.border_width_px = 0,
+			.variant = hp_label
 		},
 		Blueprint::Widget{
 			.x = 4, .y = 52, .w = 11, .h = 2,
-			.variant = left_hud_variant_2
-		},
-		Blueprint::Widget{
-			.x = 4, .y = 54, .w = 11, .h = 2,
-			.variant = left_hud_variant_3
+			.variant = hp_progress_bar
 		},
 		Blueprint::Widget{
 			.x = 4, .y = 56, .w = 11, .h = 2,
+			.border_width_px = 0,
+			.variant = dash_label
+		},
+		Blueprint::Widget{
+			.x = 4, .y = 58, .w = 11, .h = 2,
 			.border_width_px = 1,
-			.variant = left_hud_variant_4
+			.variant = dash_progress_bar
 		}
 	}
 };
