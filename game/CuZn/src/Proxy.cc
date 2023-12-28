442147e70c05b65ed9df56e48fc8a2297350ad1a
Çağrı Uslu <uslu.cagri@gmail.com>
2023-12-28 11:56:42 +0100

    Add new game CuZn

---
+++ game/CuZn/src/Proxy.cc
@@ -0,0 +1,70 @@
+#include <m2/Proxy.h>
+
+const std::string_view m2g::game_name = "CuZn";
+const int m2g::default_game_height_m = 16;
+const bool m2g::gravity = false;
+const bool m2g::world_is_static = true;
+const bool m2g::lightning = false;
+float m2g::focus_point_height = 2.0f;
+const float m2g::xy_plane_z_component = 0.75f;
+const bool m2g::camera_is_listener = false;
+
+void* m2g::create_context() {
+	return nullptr;
+}
+void m2g::destroy_context(MAYBE void* context) {
+}
+
+m2::Key m2g::scancode_to_key(SDL_Scancode scancode) {
+	using namespace m2;
+	switch (scancode) {
+		case SDL_SCANCODE_ESCAPE:
+			return Key::MENU;
+		case SDL_SCANCODE_W:
+			return Key::UP;
+		case SDL_SCANCODE_S:
+			return Key::DOWN;
+		case SDL_SCANCODE_A:
+			return Key::LEFT;
+		case SDL_SCANCODE_D:
+			return Key::RIGHT;
+		case SDL_SCANCODE_SPACE:
+			return Key::DASH;
+		case SDL_SCANCODE_GRAVE:
+			return Key::CONSOLE;
+		case SDL_SCANCODE_RETURN:
+			return Key::ENTER;
+		case SDL_SCANCODE_BACKSPACE:
+			return Key::BACKSPACE;
+		default:
+			return Key::UNKNOWN;
+	}
+}
+
+const std::array<SDL_Scancode, static_cast<unsigned>(m2::Key::end)> m2g::key_to_scancode = {
+		SDL_SCANCODE_UNKNOWN,
+		SDL_SCANCODE_ESCAPE,
+		SDL_SCANCODE_W,
+		SDL_SCANCODE_S,
+		SDL_SCANCODE_A,
+		SDL_SCANCODE_D,
+		SDL_SCANCODE_SPACE,
+		SDL_SCANCODE_GRAVE,
+		SDL_SCANCODE_RETURN,
+		SDL_SCANCODE_BACKSPACE
+};
+
+void m2g::pre_single_player_level_init(MAYBE const std::string& name, MAYBE const m2::pb::Level& level) {}
+
+void m2g::post_single_player_level_init(MAYBE const std::string& name, MAYBE const m2::pb::Level& level) {}
+
+void m2g::post_tile_create(m2::Object& obj, pb::SpriteType sprite_type) {
+}
+
+m2::void_expected m2g::init_fg_object(m2::Object& obj, pb::ObjectType object_type) {
+	return m2::make_unexpected("Invalid object type");
+}
+
+m2::Group* m2g::create_group(MAYBE pb::GroupType group_type) {
+	return nullptr;
+}
---
+++ game/CuZn/src/ProxyUi.cc
@@ -0,0 +1,263 @@
+#include <m2/Proxy.h>
+#include <m2/Game.h>
+#include <m2/ui/widget/Text.h>
+#include <m2/ui/widget/TextInput.h>
+
+using namespace m2::ui;
+using namespace m2::ui::widget;
+using namespace m2g;
+using namespace m2g::pb;
+
+namespace {
+	auto quit_button_action = [](MAYBE const widget::Text &self) {
+		return Action::QUIT;
+	};
+}
+
+
+static TextBlueprint client_status = {
+		.initial_text = "CONNECTING",
+		.on_update = [](MAYBE const Text& self) -> std::pair<Action,std::optional<std::string>> {
+			if (GAME.client_thread().is_connected()) {
+				return std::make_pair(Action::CONTINUE, "CONNECTED");
+			} else if (GAME.client_thread().is_ready()) {
+				return std::make_pair(Action::CONTINUE, "READY");
+			} else if (GAME.client_thread().is_started()) {
+				// TODO Initialize level
+				return std::make_pair(Action::RETURN, std::nullopt);
+			} else {
+				return std::make_pair(Action::CONTINUE, "CONNECTING...");
+			}
+		}
+};
+static TextBlueprint ready_button = {
+		.initial_text = "...",
+		.on_update = [](MAYBE const Text& self) -> std::pair<Action,std::optional<std::string>> {
+			if (GAME.client_thread().is_connected()) {
+				return std::make_pair(Action::CONTINUE, "SET READY");
+			} else if (GAME.client_thread().is_ready()) {
+				return std::make_pair(Action::CONTINUE, "CLEAR READY");
+			} else {
+				return std::make_pair(Action::CONTINUE, "...");
+			}
+		},
+		.on_action = [](MAYBE const Text& self) -> Action {
+			if (GAME.client_thread().is_connected()) {
+				GAME.client_thread().set_ready_blocking(true);
+			} else if (GAME.client_thread().is_ready()) {
+				GAME.client_thread().set_ready_blocking(false);
+			}
+			return Action::CONTINUE;
+		}
+};
+static const Blueprint client_lobby = {
+		.w = 160, .h = 90,
+		.border_width_px = 0,
+		.background_color = {20, 20, 20, 255},
+		.widgets = {
+				WidgetBlueprint{
+						.x = 60, .y = 30, .w = 40, .h = 10,
+						.border_width_px = 0,
+						.variant = client_status
+				},
+				WidgetBlueprint{
+						.x = 60, .y = 50, .w = 40, .h = 10,
+						.border_width_px = 1,
+						.variant = ready_button
+				}
+		}
+};
+
+static TextBlueprint ip_label = { .initial_text = "IP" };
+static TextInputBlueprint ip_input = {};
+static TextBlueprint connect_button = {
+		.initial_text = "CONNECT",
+		.on_action = [](MAYBE const widget::Text &self) {
+			auto* ip_input_widget = self.parent().find_first_widget_of_type<TextInput>();
+			GAME.join_game(m2::mplayer::Type::TurnBased, ip_input_widget->text_input());
+			return m2::ui::State::create_execute_sync(&client_lobby);
+		}
+};
+static const Blueprint ip_port_form = {
+		.w = 160, .h = 90,
+		.border_width_px = 0,
+		.background_color = {20, 20, 20, 255},
+		.widgets = {
+				WidgetBlueprint{
+						.x = 55, .y = 30, .w = 10, .h = 10,
+						.border_width_px = 0,
+						.variant = ip_label
+				},
+				WidgetBlueprint{
+						.x = 65, .y = 30, .w = 40, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = ip_input
+				},
+				WidgetBlueprint{
+						.x = 70, .y = 50, .w = 20, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = connect_button
+				}
+		}
+};
+
+static TextBlueprint client_count_label = { .initial_text = "Client count:" };
+static TextBlueprint client_count = {
+		.initial_text = "0",
+		.on_create = [](MAYBE const Text& self) -> std::optional<std::string> {
+			GAME.host_game(m2::mplayer::Type::TurnBased, 4);
+
+			// Wait until the server is up
+			while (GAME.server_thread().is_listening() == false) { SDL_Delay(25); }
+
+			GAME.join_game(m2::mplayer::Type::TurnBased, "127.0.0.1");
+
+			// Wait until the client is connected
+			while (GAME.client_thread().is_connected() == false) { SDL_Delay(25); }
+
+			// Set client as ready
+			GAME.client_thread().set_ready_blocking(true);
+
+			return std::nullopt;
+		},
+		.on_update = [](MAYBE const Text& self) -> std::pair<Action,std::optional<std::string>> {
+			auto client_count = GAME.server_thread().client_count();
+			if (client_count < 2) {
+				return std::make_pair(Action::CONTINUE, std::to_string(client_count));
+			} else {
+				return std::make_pair(Action::CONTINUE, std::to_string(client_count) + " - START!");
+			}
+		},
+		.on_action = [](MAYBE const Text& self) -> Action {
+			if (2 <= GAME.server_thread().client_count()) {
+				if (GAME.server_thread().close_lobby()) {
+					// TODO setup game
+					return Action::RETURN;
+				} else {
+					return Action::CONTINUE;
+				}
+			} else {
+				return Action::CONTINUE;
+			}
+		}
+};
+static const Blueprint server_lobby = {
+		.w = 160, .h = 90,
+		.border_width_px = 0,
+		.background_color = {20, 20, 20, 255},
+		.widgets = {
+				WidgetBlueprint{
+						.x = 40, .y = 40, .w = 40, .h = 10,
+						.border_width_px = 0,
+						.padding_width_px = 5,
+						.variant = client_count_label
+				},
+				WidgetBlueprint{
+						.x = 80, .y = 40, .w = 40, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = client_count
+				},
+		}
+};
+
+static TextBlueprint join_button = {
+		.initial_text = "JOIN",
+		.on_action = [](MAYBE const widget::Text &self) {
+			return m2::ui::State::create_execute_sync(&ip_port_form);
+		}
+};
+static TextBlueprint host_button = {
+		.initial_text = "HOST",
+		.on_action = [](MAYBE const widget::Text &self) {
+			return m2::ui::State::create_execute_sync(&server_lobby);
+		}
+};
+static TextBlueprint quit_button = {
+		.initial_text = "QUIT",
+		.kb_shortcut = SDL_SCANCODE_Q,
+		.on_action = quit_button_action
+};
+const Blueprint main_menu_blueprint = {
+		.w = 160, .h = 90,
+		.border_width_px = 0,
+		.background_color = {20, 20, 20, 255},
+		.widgets = {
+				WidgetBlueprint{
+						.x = 70, .y = 20, .w = 20, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = join_button
+				},
+				WidgetBlueprint{
+						.x = 70, .y = 40, .w = 20, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = host_button
+				},
+				WidgetBlueprint{
+						.x = 70, .y = 60, .w = 20, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = quit_button
+				}
+		}
+};
+
+static TextBlueprint resume_button = {
+		.initial_text = "RESUME",
+		.on_action = [](MAYBE const widget::Text &self) {
+			return Action::RETURN;
+		}
+};
+static TextBlueprint pause_menu_quit_button = {
+		.initial_text = "QUIT",
+		.kb_shortcut = SDL_SCANCODE_Q,
+		.on_action = quit_button_action,
+};
+const Blueprint pause_menu_blueprint = {
+		.w = 100, .h = 100,
+		.background_color = {.r = 20, .g = 20, .b = 20, .a = 255},
+		.widgets = {
+				WidgetBlueprint{
+						.x = 45, .y = 35, .w = 10, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = resume_button
+				},
+				WidgetBlueprint{
+						.x = 45, .y = 55, .w = 10, .h = 10,
+						.border_width_px = 1,
+						.padding_width_px = 5,
+						.variant = pause_menu_quit_button
+				}
+		}
+};
+
+const Blueprint left_hud_blueprint = {
+		.w = 19, .h = 72,
+		.border_width_px = 2
+};
+
+const Blueprint right_hud_blueprint = {
+		.w = 19, .h = 72,
+		.border_width_px = 2
+};
+
+const m2::ui::Blueprint *m2g::ui::main_menu() {
+	return &main_menu_blueprint;
+}
+
+const m2::ui::Blueprint *m2g::ui::pause_menu() {
+	return &pause_menu_blueprint;
+}
+
+const m2::ui::Blueprint *m2g::ui::left_hud() {
+	return &left_hud_blueprint;
+}
+
+const m2::ui::Blueprint *m2g::ui::right_hud() {
+	return &right_hud_blueprint;
+}
