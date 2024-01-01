9671a5897f4aaafb76d323b7834f99747dfa2755
Çağrı Uslu <uslu.cagri@gmail.com>
2024-01-01 01:22:17 +0100

    CuZn add types

--- game/CuZn/src/object/HumanPlayer.cc
+++ game/CuZn/src/object/HumanPlayer.cc
@@ -5,8 +5,8 @@ m2::void_expected cuzn::init_human_player(m2::Object& obj) {
 	auto& chr = obj.add_full_character();
 	chr.add_resource(m2g::pb::MONEY, 17.0f);
 
-	auto [income_marker, _] = m2::create_object({}, m2g::pb::INCOME_LEVEL_MARKER, obj.id());
-	income_marker.add_tiny_character();
+//	auto [income_marker, _] = m2::create_object({}, m2g::pb::INCOME_LEVEL_MARKER, obj.id());
+//	income_marker.add_tiny_character();
 
 	return {};
 }
