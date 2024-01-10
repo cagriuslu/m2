262145a3a4238bcf4a4abed42d1da2f4e2a31c96
Çağrı Uslu <uslu.cagri@gmail.com>
2024-01-10 21:28:59 +0100

    CuZn add sprite types

--- game/CuZn/src/object/HumanPlayer.cc
+++ game/CuZn/src/object/HumanPlayer.cc
@@ -3,6 +3,9 @@
 
 m2::void_expected cuzn::init_human_player(m2::Object& obj) {
 	auto& chr = obj.add_full_character();
+	chr.add_resource(m2g::pb::VICTORY_POINTS, 0.0f);
+	chr.add_resource(m2g::pb::INCOME_POINTS, 0.0f);
+	chr.add_resource(m2g::pb::DEBT_POINTS, 0.0f);
 	chr.add_resource(m2g::pb::MONEY, 17.0f);
 
 //	auto [income_marker, _] = m2::create_object({}, m2g::pb::INCOME_MARKER, obj.id());
