d14cd1c92a52aa1496de8075bf1fab76084efa82
Çağrı Uslu <uslu.cagri@gmail.com>
2024-02-03 12:47:49 +0100

    Add scrolling to ImageSelection

--- src/ui/widget/ImageSelection.cc
+++ src/ui/widget/ImageSelection.cc
@@ -36,6 +36,19 @@ Action ImageSelection::on_event(Events& events) {
 		if (0 < _selection) {
 			select(_selection - 1);
 		}
+	} else {
+		// Check if scrolled
+		if (auto scroll_amount = events.pop_mouse_wheel_vertical_scroll(rect); 0 < scroll_amount) {
+			auto min_scroll_amount = std::min(static_cast<size_t>(scroll_amount), image_selection.list.size() - _selection - 1);
+			if (min_scroll_amount) {
+				return select(_selection + min_scroll_amount);
+			}
+		} else if (scroll_amount < 0) {
+			auto min_scroll_amount = std::min(static_cast<unsigned>(-scroll_amount), _selection);
+			if (min_scroll_amount) {
+				return select(_selection - min_scroll_amount);
+			}
+		}
 	}
 	return Action::CONTINUE;
 }
