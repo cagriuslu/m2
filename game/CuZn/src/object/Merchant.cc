b8f3c3c5baac070b0ccadbb7d6b0ab013fb98ab4
Çağrı Uslu <uslu.cagri@gmail.com>
2024-02-16 22:27:37 +0100

    CuZn Add merchant resource graphic

--- game/CuZn/src/object/Merchant.cc
+++ game/CuZn/src/object/Merchant.cc
@@ -11,5 +11,16 @@ void cuzn::init_merchant(m2::Object &obj) {
 		// Set the sprite
 		g.sprite = (it != c.end_items()) ? &GAME.get_sprite(it->game_sprite()) : nullptr;
 	};
-	gfx.on_draw = m2::Graphic::default_draw;
+	gfx.on_draw = [&c = chr, pos = obj.position](m2::Graphic& g) {
+		// First, draw the merchant sprite
+		m2::Graphic::default_draw(g);
+
+		// Second, draw the beer barrl if exists
+		auto beer_barrel_count = c.get_resource(m2g::pb::BEER_BARREL_COUNT);
+		if (beer_barrel_count == 1.0f) {
+			draw_real_2d(pos + m2::VecF{1, 1}, GAME.get_sprite(m2g::pb::BEER_BARREL), m2::IsForegroundCompanion{false}, 0.0f);
+		} else if (beer_barrel_count != 0.0f) {
+			throw M2ERROR(std::string("Invalid beer barrel count: ") + std::to_string(beer_barrel_count));
+		}
+	};
 }
