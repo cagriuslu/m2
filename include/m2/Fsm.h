724f74d12c097169453fd701d95e7c785eaff224
Çağrı Uslu <uslu.cagri@gmail.com>
2024-02-23 16:02:58 +0300

    Reorganization

--- include/m2/Fsm.h
+++ include/m2/Fsm.h
@@ -29,54 +29,59 @@ namespace m2 {
 
 	/// Base class of all FSMs
 	/// State should contain information that effects the behavior drastically
-	/// Other data can be stored as derived class member
+	/// Other data can be stored as derived class' member
 	template <typename StateEnum, typename SignalT>
 	class FsmBase {
 		static_assert(std::is_base_of_v<FsmSignalBase,SignalT> == true);
 		StateEnum _state;
 		float _alarm{NAN};
 
-	public:
+	protected:
 		explicit FsmBase(StateEnum initial_state) : _state(std::move(initial_state)) {}
+	public:
 		virtual ~FsmBase() = default;
 
-		/// This function needs to be called after construction because handle_signal virtual function
-		/// cannot be called during construction.
+		/// This function should be called after construction because handle_signal virtual function is unavailable
+		/// during construction.
 		void init() {
 			signal(SignalT{FsmSignalType::EnterState});
 		}
+
+		/// Arm the alarm
 		void arm(float duration_s) {
 			if (duration_s < 0.0f) {
 				throw M2ERROR("Invalid alarm duration");
 			}
 			_alarm = duration_s;
 		}
+		/// Disarm the alarm
 		void disarm() {
 			_alarm = NAN;
 		}
-		void signal(const SignalT& s) {
-			auto optional_next_state = handle_signal(s);
-			if (optional_next_state) {
-				handle_signal(SignalT{FsmSignalType::ExitState});
-				_state = *optional_next_state;
-				signal(SignalT{FsmSignalType::EnterState});
-			}
-		}
+		/// Report the time passed to the state machine
 		void time(float delta_time) {
 			if (not isnan(_alarm)) {
-				_alarm -= delta_time;
-				if (_alarm <= 0.0f) {
+				if (_alarm -= delta_time; _alarm <= 0.0f) {
 					_alarm = NAN;
 					signal(SignalT{FsmSignalType::Alarm});
 				}
 			}
 		}
 
+		/// Send a signal to the state machine
+		void signal(const SignalT& s) {
+			if (auto optional_next_state = handle_signal(s); optional_next_state) {
+				handle_signal(SignalT{FsmSignalType::ExitState});
+				_state = *optional_next_state;
+				signal(SignalT{FsmSignalType::EnterState});
+			}
+		}
+
 	protected:
 		const StateEnum& state() const { return _state; }
 
-		/// If next state is returned, ExitState and EnterState signals will be called
-		/// If these signals are not necessary, {} should be returned
+		/// If StateEnum is returned, ExitState and EnterState signals will be called.
+		/// If std::nullopt is returned, the state stays the same.
 		virtual std::optional<StateEnum> handle_signal(const SignalT& s) = 0;
 	};
 }
