8451b5095dd1b40eef26dc5e339ba98b2a67994f
Çağrı Uslu <uslu.cagri@gmail.com>
2024-10-03 20:41:22 +0200

    CuZn simplify left hud

--- game/CuZn/src/ui/LeftHud.cc
+++ game/CuZn/src/ui/LeftHud.cc
@@ -18,7 +18,7 @@ using namespace m2g::pb;
 namespace {
 	const auto build_button = TextBlueprint{
 		.text = "Build",
-		.wrapped_font_size_in_units = 2.5f,
+		.wrapped_font_size_in_units = 1.75f,
 		.on_action = [](MAYBE const Text& self) -> Action {
 			if (auto build_prerequisite = can_player_attempt_to_build(M2_PLAYER.character())) {
 				m2g::Proxy::get_instance().user_journey.emplace(BuildJourney{});
@@ -31,7 +31,7 @@ namespace {
 
 	const auto develop_button = TextBlueprint{
 		.text = "Develop",
-		.wrapped_font_size_in_units = 2.5f,
+		.wrapped_font_size_in_units = 1.75f,
 		.on_action = [](MAYBE const Text& self) -> Action {
 			if (auto develop_prerequisite = can_player_attempt_to_develop(M2_PLAYER.character())) {
 				m2g::Proxy::get_instance().user_journey.emplace(DevelopJourney{});
@@ -44,7 +44,7 @@ namespace {
 
 	const auto loan_button = TextBlueprint{
 		.text = "Loan",
-		.wrapped_font_size_in_units = 2.5f,
+		.wrapped_font_size_in_units = 1.75f,
 		.on_action = [](MAYBE const m2::ui::widget::Text& self) -> m2::ui::Action {
 			if (auto loan_prerequisite = can_player_attempt_to_loan(M2_PLAYER.character())) {
 				execute_loan_journey();
@@ -57,7 +57,7 @@ namespace {
 
 	const auto network_button = TextBlueprint{
 		.text = "Network",
-		.wrapped_font_size_in_units = 2.5f,
+		.wrapped_font_size_in_units = 1.75f,
 		.on_action = [](MAYBE const Text& self) -> Action {
 			if (auto network_prerequisite = can_player_attempt_to_network(M2_PLAYER.character())) {
 				m2g::Proxy::get_instance().user_journey.emplace(NetworkJourney{});
@@ -70,7 +70,7 @@ namespace {
 
 	const auto pass_button = TextBlueprint{
 		.text = "Pass",
-		.wrapped_font_size_in_units = 2.5f,
+		.wrapped_font_size_in_units = 1.75f,
 		.on_action = [](MAYBE const m2::ui::widget::Text& self) -> m2::ui::Action {
 			if (M2_GAME.is_our_turn()) {
 				execute_pass_journey();
@@ -81,7 +81,7 @@ namespace {
 
 	const auto scout_button = TextBlueprint{
 		.text = "Scout",
-		.wrapped_font_size_in_units = 2.5f,
+		.wrapped_font_size_in_units = 1.75f,
 		.on_action = [](MAYBE const m2::ui::widget::Text& self) -> m2::ui::Action {
 			if (auto scout_prerequisite = can_player_attempt_to_scout(M2_PLAYER.character())) {
 				execute_scout_journey();
@@ -94,7 +94,7 @@ namespace {
 
 	const auto sell_button = TextBlueprint{
 		.text = "Sell",
-		.wrapped_font_size_in_units = 2.5f,
+		.wrapped_font_size_in_units = 1.75f,
 		.on_action = [](MAYBE const Text& self) -> Action {
 			if (auto sell_prerequisite = can_player_attempt_to_sell(M2_PLAYER.character())) {
 				m2g::Proxy::get_instance().main_journeys.emplace(SellJourney{});
@@ -113,13 +113,12 @@ const PanelBlueprint left_hud_blueprint = {
 	.background_color = {0, 0, 0, 255},
 	.widgets = {
 		WidgetBlueprint{
-			.x = 0, .y = 2, .w = 19, .h = 8,
+			.x = 0, .y = 0, .w = 19, .h = 4,
 			.border_width = 0,
 			.variant = TextBlueprint{
-				.horizontal_alignment = m2::ui::TextHorizontalAlignment::LEFT,
 				.wrapped_font_size_in_units = 2.5f,
 				.on_create = [](MAYBE Text& self) {
-					auto text = std::string{"Your color: "} + generate_player_name(M2_GAME.self_index());
+					auto text = generate_player_name(M2_GAME.self_index());
 					self.set_text(text);
 					self.set_color(generate_player_color(M2_GAME.self_index()));
 					return make_continue_action();
@@ -127,62 +126,45 @@ const PanelBlueprint left_hud_blueprint = {
 			}
 		},
 		WidgetBlueprint{
-			.x = 2, .y = 9, .w = 15, .h = 6,
-			.border_width = 0,
-			.variant = TextBlueprint{
-				.horizontal_alignment = m2::ui::TextHorizontalAlignment::CENTER,
-				.wrapped_font_size_in_units = 2.5f,
-				.on_update = [](MAYBE Text& self) {
-					if (M2_GAME.is_our_turn()) {
-						self.set_text("Your turn");
-					} else {
-						self.set_text("Wait turn");
-					}
-					return make_continue_action();
-				}
-			}
-		},
-		WidgetBlueprint{
 			.name = "BuildButton",
 			.initially_enabled = false,
-			.x = 2, .y = 16, .w = 15, .h = 6,
+			.x = 2, .y = 5, .w = 15, .h = 4,
 			.variant = build_button
 		},
-
 		WidgetBlueprint{
 			.name = "SellButton",
 			.initially_enabled = false,
-			.x = 2, .y = 23, .w = 15, .h = 6,
+			.x = 2, .y = 10, .w = 15, .h = 4,
 			.variant = sell_button
 		},
 		WidgetBlueprint{
 			.name = "NetworkButton",
 			.initially_enabled = false,
-			.x = 2, .y = 30, .w = 15, .h = 6,
+			.x = 2, .y = 15, .w = 15, .h = 4,
 			.variant = network_button
 		},
 		WidgetBlueprint{
 			.name = "LoanButton",
 			.initially_enabled = false,
-			.x = 2, .y = 37, .w = 15, .h = 6,
+			.x = 2, .y = 20, .w = 15, .h = 4,
 			.variant = loan_button
 		},
 		WidgetBlueprint{
 			.name = "DevelopButton",
 			.initially_enabled = false,
-			.x = 2, .y = 44, .w = 15, .h = 6,
+			.x = 2, .y = 25, .w = 15, .h = 4,
 			.variant = develop_button
 		},
 		WidgetBlueprint{
 			.name = "ScoutButton",
 			.initially_enabled = false,
-			.x = 2, .y = 51, .w = 15, .h = 6,
+			.x = 2, .y = 30, .w = 15, .h = 4,
 			.variant = scout_button
 		},
 		WidgetBlueprint{
 			.name = "PassButton",
 			.initially_enabled = false,
-			.x = 2, .y = 58, .w = 15, .h = 6,
+			.x = 2, .y = 35, .w = 15, .h = 4,
 			.variant = pass_button
 		},
 	}
