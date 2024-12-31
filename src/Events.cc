6cc19455ee3b45f388e396f9a615746d58c7a858
Çağrı Uslu <uslu.cagri@gmail.com>
2024-12-31 16:03:05 +0100

    Add postponing of future events if key/button up/down event is encountered

--- src/Events.cc
+++ src/Events.cc
@@ -12,13 +12,21 @@ namespace {
 	std::optional<VecF> secondary_selection_position_1_m, secondary_selection_position_2_m;
 }  // namespace
 
-void m2::Events::clear() { *this = Events(); }
+void Events::clear() { *this = Events(); }
 
-bool m2::Events::gather() {
+bool Events::gather() {
 	const uint32_t mouseStateBitmask = SDL_GetMouseState(&_mouse_position.x, &_mouse_position.y);
-	mouse_buttons_down[u(m2::MouseButton::PRIMARY)] = mouseStateBitmask & SDL_BUTTON(SDL_BUTTON_LEFT);
-	mouse_buttons_down[u(m2::MouseButton::SECONDARY)] = mouseStateBitmask & SDL_BUTTON(SDL_BUTTON_RIGHT);
-	mouse_buttons_down[u(m2::MouseButton::MIDDLE)] = mouseStateBitmask & SDL_BUTTON(SDL_BUTTON_MIDDLE);
+	mouse_buttons_down[u(MouseButton::PRIMARY)] = mouseStateBitmask & SDL_BUTTON(SDL_BUTTON_LEFT);
+	mouse_buttons_down[u(MouseButton::SECONDARY)] = mouseStateBitmask & SDL_BUTTON(SDL_BUTTON_RIGHT);
+	mouse_buttons_down[u(MouseButton::MIDDLE)] = mouseStateBitmask & SDL_BUTTON(SDL_BUTTON_MIDDLE);
+
+	// Current Events architecture does not store the relative ordering of the events that happens back to back during
+	// one frame. This is not a problem for most events except key up/down and mouse button up/down events. Since we
+	// don't store the relative ordering of the events, if a key is pressed and released before the next frame, we can't
+	// tell which happened before the other. There are multiple strategies of dealing with this. Caching the event for
+	// later, or storing the order of the events are valid strategies, but caching is complex to implement here, and
+	// storing the ordering of the events would complicate the game code. The easiest to implement solution is to just
+	// postponing the future events to the future frames if a key/button up/down event is encountered.
 
 	bool mouse_moved = false;
 	SDL_Event e;
@@ -46,12 +54,14 @@ bool m2::Events::gather() {
 					} else {
 						LOG_WARN("UI key press count limit exceeded");
 					}
+					goto postponeFutureEvents; // Read the note above the while loop
 				}
 				break;
 			case SDL_KEYUP:
 				if (e.key.repeat == 0) {
 					key_release_count++;
 					keys_released[u(M2G_PROXY.scancode_to_key(e.key.keysym.scancode))]++;
+					goto postponeFutureEvents; // Read the note above the while loop
 				}
 				break;
 			case SDL_MOUSEMOTION:
@@ -59,7 +69,7 @@ bool m2::Events::gather() {
 				break;
 			case SDL_MOUSEBUTTONDOWN:
 				mouse_button_press_count++;
-				mouse_buttons_pressed[u(m2::button_to_mouse_button(e.button.button))]++;
+				mouse_buttons_pressed[u(button_to_mouse_button(e.button.button))]++;
 				if (primary_selection_screen_rect_px &&
 				    peek_mouse_button_press(MouseButton::PRIMARY, *primary_selection_screen_rect_px)) {
 					primary_selection_position_1_m = M2_GAME.MousePositionWorldM();
@@ -74,10 +84,10 @@ bool m2::Events::gather() {
 					secondary_selection_position_1_m = M2_GAME.MousePositionWorldM();
 					secondary_selection_position_2_m = std::nullopt;
 				}
-				break;
+				goto postponeFutureEvents; // Read the note above the while loop
 			case SDL_MOUSEBUTTONUP:
 				mouse_button_release_count++;
-				mouse_buttons_released[u(m2::button_to_mouse_button(e.button.button))]++;
+				mouse_buttons_released[u(button_to_mouse_button(e.button.button))]++;
 				if (primary_selection_screen_rect_px &&
 				    peek_mouse_button_release(MouseButton::PRIMARY, *primary_selection_screen_rect_px)) {
 					primary_selection_position_2_m = M2_GAME.MousePositionWorldM();
@@ -90,7 +100,7 @@ bool m2::Events::gather() {
 					primary_selection_position_2_m = std::nullopt;
 					secondary_selection_position_2_m = M2_GAME.MousePositionWorldM();
 				}
-				break;
+				goto postponeFutureEvents; // Read the note above the while loop
 			case SDL_MOUSEWHEEL:
 				mouse_wheel_vertical_scroll_count += e.wheel.y;
 				mouse_wheel_horizontal_scroll_count += e.wheel.x;
@@ -112,13 +122,14 @@ bool m2::Events::gather() {
 				break;
 		}
 	}
+	postponeFutureEvents:
 
 	int keyCount = 0;
 	const uint8_t* raw_keyboard_state = SDL_GetKeyboardState(&keyCount);
 	for (int i = 0; i < keyCount; i++) {
 		sdl_keys_down[i] = raw_keyboard_state[i];
 	}
-	for (unsigned i = 1; i < u(m2::Key::end); i++) {
+	for (unsigned i = 1; i < u(Key::end); i++) {
 		auto scancode = M2G_PROXY.key_to_scancode[i];
 		keys_down[i] = (scancode != SDL_SCANCODE_UNKNOWN) && raw_keyboard_state[scancode];
 	}
@@ -131,13 +142,13 @@ bool m2::Events::gather() {
 	    (std::find(mouse_buttons_down.begin(), mouse_buttons_down.end(), true) != mouse_buttons_down.end());
 }
 
-bool m2::Events::pop_quit() {
+bool Events::pop_quit() {
 	auto value = quit;
 	quit = false;
 	return value;
 }
 
-bool m2::Events::pop_window_resize() {
+bool Events::pop_window_resize() {
 	if (window_resize) {
 		window_resize = false;
 		return true;
@@ -145,7 +156,7 @@ bool m2::Events::pop_window_resize() {
 	return false;
 }
 
-bool m2::Events::pop_key_press(m2::Key k) {
+bool Events::pop_key_press(Key k) {
 	if (keys_pressed[u(k)]) {
 		keys_pressed[u(k)]--;
 		key_press_count--;
@@ -155,7 +166,7 @@ bool m2::Events::pop_key_press(m2::Key k) {
 	}
 }
 
-bool m2::Events::pop_ui_key_press(SDL_Scancode scode) {
+bool Events::pop_ui_key_press(SDL_Scancode scode) {
 	for (auto it = ui_keys_pressed.begin(); it != ui_keys_pressed.end(); it++) {
 		if (*it == scode) {
 			ui_keys_pressed.erase(it);
@@ -165,7 +176,7 @@ bool m2::Events::pop_ui_key_press(SDL_Scancode scode) {
 	return false;
 }
 
-bool m2::Events::pop_key_release(m2::Key k) {
+bool Events::pop_key_release(Key k) {
 	if (keys_released[u(k)]) {
 		keys_released[u(k)]--;
 		key_release_count--;
@@ -175,8 +186,8 @@ bool m2::Events::pop_key_release(m2::Key k) {
 	}
 }
 
-bool m2::Events::peek_mouse_button_press(m2::MouseButton mb) { return mouse_buttons_pressed[u(mb)]; }
-bool m2::Events::pop_mouse_button_press(m2::MouseButton mb) {
+bool Events::peek_mouse_button_press(MouseButton mb) { return mouse_buttons_pressed[u(mb)]; }
+bool Events::pop_mouse_button_press(MouseButton mb) {
 	if (mouse_buttons_pressed[u(mb)]) {
 		mouse_buttons_pressed[u(mb)]--;
 		mouse_button_press_count--;
@@ -188,7 +199,7 @@ bool m2::Events::pop_mouse_button_press(m2::MouseButton mb) {
 bool Events::peek_mouse_button_press(MouseButton mb, const RectI& rect) {
 	return mouse_buttons_pressed[u(mb)] && rect.contains(mouse_position());
 }
-bool m2::Events::pop_mouse_button_press(m2::MouseButton mb, const RectI& rect) {
+bool Events::pop_mouse_button_press(MouseButton mb, const RectI& rect) {
 	if (mouse_buttons_pressed[u(mb)] && rect.contains(mouse_position())) {
 		mouse_buttons_pressed[u(mb)]--;
 		mouse_button_press_count--;
@@ -197,14 +208,14 @@ bool m2::Events::pop_mouse_button_press(m2::MouseButton mb, const RectI& rect) {
 		return false;
 	}
 }
-void m2::Events::clear_mouse_button_presses(const RectI& rect) {
+void Events::clear_mouse_button_presses(const RectI& rect) {
 	if (rect.contains(mouse_position())) {
 		mouse_buttons_pressed = {};
 		mouse_button_press_count = 0;
 	}
 }
-bool m2::Events::peek_mouse_button_release(m2::MouseButton mb) { return mouse_buttons_released[u(mb)]; }
-bool m2::Events::pop_mouse_button_release(m2::MouseButton mb) {
+bool Events::peek_mouse_button_release(MouseButton mb) { return mouse_buttons_released[u(mb)]; }
+bool Events::pop_mouse_button_release(MouseButton mb) {
 	if (mouse_buttons_released[u(mb)]) {
 		mouse_buttons_released[u(mb)]--;
 		mouse_button_release_count--;
@@ -216,7 +227,7 @@ bool m2::Events::pop_mouse_button_release(m2::MouseButton mb) {
 bool Events::peek_mouse_button_release(MouseButton mb, const RectI& rect) {
 	return mouse_buttons_released[u(mb)] && rect.contains(mouse_position());
 }
-bool m2::Events::pop_mouse_button_release(MouseButton mb, const RectI& rect) {
+bool Events::pop_mouse_button_release(MouseButton mb, const RectI& rect) {
 	if (mouse_buttons_released[u(mb)] && rect.contains(mouse_position())) {
 		mouse_buttons_released[u(mb)]--;
 		mouse_button_release_count--;
@@ -225,19 +236,19 @@ bool m2::Events::pop_mouse_button_release(MouseButton mb, const RectI& rect) {
 		return false;
 	}
 }
-void m2::Events::clear_mouse_button_releases(const RectI& rect) {
+void Events::clear_mouse_button_releases(const RectI& rect) {
 	if (rect.contains(mouse_position())) {
 		mouse_buttons_released = {};
 		mouse_button_release_count = 0;
 	}
 }
 
-int32_t m2::Events::pop_mouse_wheel_vertical_scroll() {
+int32_t Events::pop_mouse_wheel_vertical_scroll() {
 	auto value = mouse_wheel_vertical_scroll_count;
 	mouse_wheel_vertical_scroll_count = 0;
 	return value;
 }
-int32_t m2::Events::pop_mouse_wheel_vertical_scroll(const RectI& rect) {
+int32_t Events::pop_mouse_wheel_vertical_scroll(const RectI& rect) {
 	if (rect.contains(mouse_position())) {
 		auto value = mouse_wheel_vertical_scroll_count;
 		mouse_wheel_vertical_scroll_count = 0;
@@ -245,12 +256,12 @@ int32_t m2::Events::pop_mouse_wheel_vertical_scroll(const RectI& rect) {
 	}
 	return 0;
 }
-int32_t m2::Events::pop_mouse_wheel_horizontal_scroll() {
+int32_t Events::pop_mouse_wheel_horizontal_scroll() {
 	auto value = mouse_wheel_horizontal_scroll_count;
 	mouse_wheel_horizontal_scroll_count = 0;
 	return value;
 }
-int32_t m2::Events::pop_mouse_wheel_horizontal_scroll(const RectI& rect) {
+int32_t Events::pop_mouse_wheel_horizontal_scroll(const RectI& rect) {
 	if (rect.contains(mouse_position())) {
 		auto value = mouse_wheel_horizontal_scroll_count;
 		mouse_wheel_horizontal_scroll_count = 0;
@@ -258,14 +269,14 @@ int32_t m2::Events::pop_mouse_wheel_horizontal_scroll(const RectI& rect) {
 	}
 	return 0;
 }
-void m2::Events::clear_mouse_wheel_scrolls(const RectI& rect) {
+void Events::clear_mouse_wheel_scrolls(const RectI& rect) {
 	if (rect.contains(mouse_position())) {
 		mouse_wheel_vertical_scroll_count = 0;
 		mouse_wheel_horizontal_scroll_count = 0;
 	}
 }
 
-std::optional<std::string> m2::Events::pop_text_input() {
+std::optional<std::string> Events::pop_text_input() {
 	auto str = text_input.str();
 	if (not str.empty()) {
 		text_input = std::stringstream();
@@ -275,15 +286,15 @@ std::optional<std::string> m2::Events::pop_text_input() {
 	}
 }
 
-bool m2::Events::is_sdl_key_down(SDL_Scancode sc) const { return sdl_keys_down[sc]; }
-bool m2::Events::is_key_down(Key k) const { return keys_down[u(k)]; }
-bool m2::Events::is_mouse_button_down(MouseButton mb) const { return mouse_buttons_down[u(mb)]; }
-void m2::Events::clear_mouse_button_down(const RectI& rect) {
+bool Events::is_sdl_key_down(SDL_Scancode sc) const { return sdl_keys_down[sc]; }
+bool Events::is_key_down(Key k) const { return keys_down[u(k)]; }
+bool Events::is_mouse_button_down(MouseButton mb) const { return mouse_buttons_down[u(mb)]; }
+void Events::clear_mouse_button_down(const RectI& rect) {
 	if (rect.contains(mouse_position())) {
 		mouse_buttons_down = {};
 	}
 }
-m2::VecI m2::Events::mouse_position() const { return _mouse_position; }
+VecI Events::mouse_position() const { return _mouse_position; }
 
 std::pair<std::optional<VecF>, std::optional<VecF>> Events::primary_selection_position_m() const {
 	return std::make_pair(primary_selection_position_1_m, primary_selection_position_2_m);
