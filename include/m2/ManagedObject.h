175785da4433499aaa979ae61ccbe9107b8f759e
Çağrı Uslu <uslu.cagri@gmail.com>
2025-08-19 23:30:15 +0200

    Add managed object utility

---
+++ include/m2/ManagedObject.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <functional>
+#include <type_traits>
+
+namespace m2 {
+	/// \brief A utility for wrapping an object with a manager.
+	/// \details Allows the contained object to be modified before writing, or side-effects to be incurred on each write.
+	template <typename T>
+	class ManagedObject final {
+		static_assert(not std::is_reference_v<T>);
+
+		T _obj;
+		std::function<void(T&)> _writeHook;
+
+	public:
+		explicit ManagedObject(std::function<void(T&)> writeHook, T&& obj) : _obj(std::move(obj)), _writeHook(std::move(writeHook)) {}
+
+		template <typename... TArgs>
+		explicit ManagedObject(std::function<void(T&)> writeHook, TArgs... args) : _obj(args...), _writeHook(std::move(writeHook)) {}
+
+		const T& Get() const { return _obj; }
+
+		void Set(T newValue) {
+			_obj = std::move(newValue);
+			if (_writeHook) {
+				_writeHook(_obj);
+			}
+		}
+		void Set(const T& newValue) {
+			_obj = newValue;
+			if (_writeHook) {
+				_writeHook(_obj);
+			}
+		}
+		void Set(T&& newValue) {
+			_obj = std::move(newValue);
+			if (_writeHook) {
+				_writeHook(_obj);
+			}
+		}
+		template <typename... TArgs>
+		void Set(TArgs... args) {
+			_obj = T{args...};
+			if (_writeHook) {
+				_writeHook(_obj);
+			}
+		}
+		void Mutate(const std::function<void(T&)>& mutator) {
+			if (mutator) {
+				mutator(_obj);
+			}
+			if (_writeHook) {
+				_writeHook(_obj);
+			}
+		}
+	};
+}
