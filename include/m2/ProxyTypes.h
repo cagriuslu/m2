116e9656752cf2ad5c70a5c413eb843d1ca8effb
Çağrı Uslu <uslu.cagri@gmail.com>
2025-09-14 06:29:51 +0200

    Add VecE

--- include/m2/ProxyTypes.h
+++ include/m2/ProxyTypes.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <m2/math/primitives/Exact.h>
 #include <m2/math/primitives/Float.h>
+#include <m2/math/VecE.h>
+#include <m2/math/VecF.h>
 #include <m2/BuildOptions.h>
 
 namespace m2 {
 	using FE = std::conditional_t<GAME_IS_DETERMINISTIC, Exact, Float>;
+	using VecFE = std::conditional_t<GAME_IS_DETERMINISTIC, VecE, VecF>;
 }
---
+++ include/m2/math/VecE.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <m2/math/primitives/Exact.h>
+
+namespace m2 {
+	class VecE {
+		Exact _x, _y;
+
+	public:
+		// Constructors
+
+		VecE() = default;
+	};
+}
