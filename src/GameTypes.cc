c8e0e01881d5c42d7240cf8c37a3369d4f5b8478
Çağrı Uslu <uslu.cagri@gmail.com>
2025-09-17 00:35:34 +0200

    Make Level blueprint deterministic

--- src/GameTypes.cc
+++ src/GameTypes.cc
@@ -1,10 +1,19 @@
 #include <m2/GameTypes.h>
+#include <m2/BuildOptions.h>
 
 m2::IFE::IFE(const pb::IFE& ife) {
 	if (ife.has_i()) {
 		_value = ife.i();
-	} else if (ife.has_fe()) {
-		_value = FE::FromProtobufRepresentation(ife.fe().value_e6());
+	} else if (ife.has_f()) {
+		if constexpr (GAME_IS_DETERMINISTIC) {
+			throw M2_ERROR("Deterministic game shouldn't contain an IFE of type float");
+		}
+		_value = FE{ife.f()};
+	} else if (ife.has_e6()) {
+		if constexpr (not GAME_IS_DETERMINISTIC) {
+			throw M2_ERROR("Non-deterministic game shouldn't contain an IFE of type exact");
+		}
+		_value = FE::FromProtobufRepresentation(ife.e6());
 	} else {
 		_value = 0;
 	}
